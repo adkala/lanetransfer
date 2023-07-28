cd8f63a8a4adf453e0e72560e40e575f27fb8cbc
adkala <addikala@berkeley.edu>
2023-07-27 20:33:53 -0700

    fixes

--- models.py
+++ models.py
@@ -3,18 +3,26 @@ import torch.nn.functional as F
 from torch import nn, Tensor
 from torchvision.models import vgg16, VGG16_Weights
 
+def _replaceLayer(x):
+    if isinstance(x, nn.MaxPool2d):
+        return nn.Upsample(scale_factor=2)
+    elif isinstance(x, nn.Conv2d):
+        return nn.Conv2d(x.out_channels, x.in_channels, kernel_size=x.kernel_size, stride=x.stride, padding=x.padding)
+    else:
+        return x
+
 class SymVGG16(nn.Module):
     def __init__(self, num_classes=3, pretrained=True):
         super().__init__()
         f = vgg16(weights = VGG16_Weights.DEFAULT if pretrained else None).features[:-1]
-        self.r = [replaceLayer(x) for x in f[-2:1:-1]]
+        r = [_replaceLayer(x) for x in f[-2:1:-1]]
              
-        self.m = nn.Sequential(*f, *r) # symmetrically concatentated vgg
+        self.m = nn.Sequential(*f, *r) # symmetrically concatenated vgg
         
         self.fc = nn.Sequential( # per-pixel MLP layers (64 -> 128 -> 3)
             nn.Conv2d(64, 128, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(128, num_classes, kernel_size=1, stride=1, padding=0),
             nn.ReLU()
         )
 
